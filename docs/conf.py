cb1b4e63143f656623e3037540a8da8f7324e045
Troy N. Smith <troy.smith@uga.edu>
2026-09-24 11:46:53 -0600

    Add sphinx autodoc typehints

--- docs/conf.py
+++ docs/conf.py
@@ -39,9 +39,8 @@ extensions = [
     "sphinx.ext.autosummary",
     "sphinx.ext.napoleon",
     "sphinxcontrib.bibtex",
+    "sphinx_autodoc_typehints",
 ]
-
-# sphinx_autodoc_typehints
 
 autodoc_default_options = {
     "members": True,
@@ -60,7 +59,6 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-""" # Saving for a future PR
 # Autodoc type hints configuration
 autodoc_typehints = "description"
 simplify_optional_unions = (
@@ -69,7 +67,6 @@ simplify_optional_unions = (
 typehints_use_signature = True  # Show parameter types in signature
 typehints_use_signature_return = True  # Show return type in signature
 typehints_defaults = "braces"  # "param (type, (default: value)) -- description"
-"""
 
 
 # -- Options for HTML output -------------------------------------------------
